9bef1a1848e796aebcb665bb5eaefe953c6e1d55
aron-c <610322518@qq.com>
2017-03-31 13:47:44 +0800

    change of test2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+
+// function tabNav(num) {
+
+// 	var i = 1
+	
+// 	for (i; i <6; i++) {
+		
+// 		document.getElementById('cnt_'+i).style.display='none';
+		
+// 		console.log(i);
+
+// 	}// body...
+
+// 	document.getElementById('cnt_'+num).style.display='block';
+
+// 	console.log(num);
+
+// }
